7b2252b322143b3f0b423438ccd29c3caf423161
Lord of Shines <101048219+Shiny-Miner@users.noreply.github.com>
2024-08-31 03:15:04 +0530

    Update and rename Pokemon _storage_system to Pokemon _storage_system.c

--- src/Pokemon _storage_system.c
+++ src/Pokemon _storage_system.c
@@ -12,5 +12,24 @@
 #include "../include/constants/rgb.h"
 #include "../include/constants/songs.h"
 #include "../include/constants/pokemon_icon.h"
+
 gKeyRepeatContinueDelay = 3;
 gKeyRepeatStartDelay = 30;
+
+#define INSTANT_BOX 0
+
+#define CURSOR_MOVE_BASE_STEPS_WRAP 3
+#define CURSOR_MOVE_BASE_STEPS_NO_WRAP 3
+
+#define BOX_SCROLL_SPEED_FACTOR 4
+#define BOX_SCROLL_ARROWS_START_X_DELTA 12
+
+#define BOX_SCROLL_SPEED(input) ((input) * 6 * BOX_SCROLL_SPEED_FACTOR)
+
+#define PARTY_MENU_SCROLL_DELTA 2
+#define PARTY_MENU_ADJUSTED_SCROLL_TIMER (20/PARTY_MENU_SCROLL_DELTA)
+
+#define COMPACT_PARTY_SPRITES_DELTA 2
+
+#define MON_HAND_DELTA 2
+
