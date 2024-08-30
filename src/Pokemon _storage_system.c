59a25eb6e071411e876485c576122143792d50c2
Lord of Shines <101048219+Shiny-Miner@users.noreply.github.com>
2024-08-31 03:31:01 +0530

    Update Pokemon _storage_system.c

--- src/Pokemon _storage_system.c
+++ src/Pokemon _storage_system.c
@@ -33,3 +33,63 @@ gKeyRepeatStartDelay = 30;
 
 #define MON_HAND_DELTA 2
 
+const u16 ALIGNED(4) *palettes;
+u16 ALIGNED(4) wallpaperTilemap[360];
+s16 iconScrollDistance;
+static void CreateInstantIncomingBoxTitle(u8 boxId, s8 direction);
+static void SpriteCB_InstantIncomingBoxTitle(struct Sprite *);
+static void SpriteCB_InstantOutgoingBoxTitle(struct Sprite *);
+
+static bool8 ShowPartyMenu(void)
+{
+  if (sStorage->partyMenuMoveTimer == PARTY_MENU_ADJUSTED_SCROLL_TIMER)
+        return FALSE;
+
+    sStorage->partyMenuUnused1--;
+sStorage->partyMenuY += PARTY_MENU_SCROLL_DELTA;
+    TilemapUtil_Move(TILEMAPID_PARTY_MENU, 3, PARTY_MENU_SCROLL_DELTA);
+TilemapUtil_Update(TILEMAPID_PARTY_MENU);
+    ScheduleBgCopyTilemapToVram(1);
+MovePartySprites(8 * PARTY_MENU_SCROLL_DELTA);
+    if (++sStorage->partyMenuMoveTimer == PARTY_MENU_ADJUSTED_SCROLL_TIMER)
+      {
+        sInPartyMenu = TRUE;
+        return FALSE;
+    }
+    else
+    {
+        return TRUE;
+    }
+}
+
+static bool8 HidePartyMenu(void)
+{
+  
+    if (sStorage->partyMenuMoveTimer != PARTY_MENU_ADJUSTED_SCROLL_TIMER)
+    {
+        sStorage->partyMenuUnused1++;
+       sStorage->partyMenuY -= PARTY_MENU_SCROLL_DELTA;
+        TilemapUtil_Move(TILEMAPID_PARTY_MENU, 3, -PARTY_MENU_SCROLL_DELTA);
+        TilemapUtil_Update(TILEMAPID_PARTY_MENU);
+FillBgTilemapBufferRect_Palette0(1, 0x100, 10, sStorage->partyMenuY, 12, PARTY_MENU_SCROLL_DELTA);
+        MovePartySprites(-8 * PARTY_MENU_SCROLL_DELTA);
+        if (++sStorage->partyMenuMoveTimer != PARTY_MENU_ADJUSTED_SCROLL_TIMER)
+          {
+            ScheduleBgCopyTilemapToVram(1);
+            return TRUE;
+        }
+        else
+        {
+            sInPartyMenu = FALSE;
+            DestroyAllPartyMonIcons();
+            CompactPartySlots();
+            // The close box button gets partially covered by
+            // the party menu, restore it
+            TilemapUtil_SetRect(TILEMAPID_CLOSE_BUTTON, 0, 0, 9, 2);
+            TilemapUtil_Update(TILEMAPID_CLOSE_BUTTON);
+            ScheduleBgCopyTilemapToVram(1);
+            return FALSE;
+        }
+    }
+    return FALSE;
+}
